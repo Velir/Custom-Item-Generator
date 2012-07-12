dd99713afad2e32abe3b7a623e385f835e055efd
TimB <TimB@cb4cd6e1-1152-384a-8fed-52b94e437518>
2012-07-12 19:01:55 +0000

    Adding an additional method to the multilist field property to return a list of ids.

--- Fields/ListTypes/CustomMultiListField.cs
+++ Fields/ListTypes/CustomMultiListField.cs
@@ -22,5 +22,41 @@ namespace CustomItemGenerator.Fields.ListTypes
 			}
 		}
 
+		/// <summary>
+		/// Returns the ID values of the field as a list of strings
+		/// </summary>
+		public List<string> Ids
+		{
+			get
+			{
+				if (field == null)
+				{
+					return new List<string>();
+				}
+
+				if (item.Fields[field.InnerField.Name] == null)
+				{
+					return new List<string>();
+				}
+
+				if (string.IsNullOrEmpty(Raw))
+				{
+					return new List<string>();
+				}
+
+				List<string> itemIds = new List<string>();
+				foreach(string id in Raw.Split('|'))
+				{
+					if(string.IsNullOrEmpty(id))
+					{
+						continue;
+					}
+
+					itemIds.Add(id);
+				}
+
+				return itemIds;
+			}
+		}
 	}
 }
