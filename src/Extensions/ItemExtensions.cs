c770e54eaed3f4267edca61b1e0fb072207bc182
Jan Hebnes <jan.hebnes@gmail.com>
2012-10-30 15:06:04 +0100

    Added Base Item Template Tweaks

---
+++ src/Extensions/ItemExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Data.Managers;
+using Sitecore.Data.Templates;
+
+namespace CustomItemGenerator.Extensions
+{
+    public static class ItemExtensions
+    {
+        public static bool IsDerivedFrom(this Item item, string templateId)
+        {
+            if (item == null)
+                return false;
+
+            if (string.IsNullOrEmpty(templateId))
+            {
+                return false;
+            }
+
+            Item templateItem = item.Database.Items[new ID(templateId)];
+            if (templateItem == null)
+            {
+                return false;
+            }
+
+            Template template = TemplateManager.GetTemplate(item);
+            if (template == null)
+            {
+                return false;
+            }
+
+            if (template.ID == templateItem.ID)
+            {
+                return true;
+            }
+
+            return template.DescendsFrom(templateItem.ID);
+        }
+    }
+}
